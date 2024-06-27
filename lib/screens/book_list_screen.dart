c148b3afa16c87cec9fa24742e53f62acd3c155d
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-06-27 22:24:27 +0700

    feat(widgets): create list book view widget

--- lib/screens/book_list_screen.dart
+++ lib/screens/book_list_screen.dart
@@ -1,10 +1,7 @@
-import 'package:bucher_palm_code/blocs/add_my_books/add_my_books_bloc.dart';
 import 'package:bucher_palm_code/blocs/get_book/get_book_bloc.dart';
 import 'package:bucher_palm_code/screens/widgets/app_input_form.dart';
-import 'package:bucher_palm_code/screens/widgets/book_card.dart';
+import 'package:bucher_palm_code/screens/widgets/list_book_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:skeletonizer/skeletonizer.dart';
 
 import '../injector.dart';
 
@@ -36,54 +33,8 @@ class BookListScreen extends StatelessWidget {
               ],
             ),
           ),
-          Expanded(
-            child: BlocBuilder<GetBookBloc, GetBookState>(
-              bloc: sl<GetBookBloc>()..add(GetBookProccessed()),
-              buildWhen: (previous, current) => previous != current,
-              builder: (context, state) {
-                /// Display error message if error occurred
-                if (state.status.isError) {
-                  return const Center(
-                    child: Text("An error occurred!"),
-                  );
-                } else if (state.status.isLoading || state.status.isInitial) {
-                  /// Display shimmer loading animation if the status is initial or loading
-                  return Skeletonizer(
-                    child: ListView.separated(
-                      padding: const EdgeInsets.all(16),
-                      itemBuilder: (context, index) => const BookCard(),
-                      separatorBuilder: (_, __) => const SizedBox(height: 16),
-                      itemCount: 32,
-                    ),
-                  );
-                }
-
-                /// Otherwise, display the book data
-                return BlocConsumer<AddMyBooksBloc, AddMyBooksState>(
-                  listener: (context, myBookState) {
-                    if (myBookState.status.isError ||
-                        myBookState.status.isSuccess) {
-                      sl<GetBookBloc>().add(GetBookProccessed());
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(myBookState.message!),
-                        ),
-                      );
-                    }
-                  },
-                  builder: (context, myBookState) {
-                    return ListView.separated(
-                      padding: const EdgeInsets.all(16),
-                      itemBuilder: (context, index) => BookCard(
-                        data: state.items.elementAt(index),
-                      ),
-                      separatorBuilder: (_, __) => const SizedBox(height: 16),
-                      itemCount: state.items.length,
-                    );
-                  },
-                );
-              },
-            ),
+          const Expanded(
+            child: ListBookView(),
           ),
         ],
       ),
---
+++ lib/screens/widgets/list_book_view.dart
@@ -0,0 +1,78 @@
+import 'package:bucher_palm_code/blocs/add_my_books/add_my_books_bloc.dart';
+import 'package:bucher_palm_code/blocs/get_book/get_book_bloc.dart';
+import 'package:bucher_palm_code/injector.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+import 'book_card.dart';
+
+class ListBookView extends StatelessWidget {
+  final bool isFavorite;
+
+  const ListBookView({super.key, this.isFavorite = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GetBookBloc, GetBookState>(
+      bloc: sl<GetBookBloc>()..add(GetBookProccessed(isFavorite: isFavorite)),
+      buildWhen: (previous, current) => previous != current,
+      builder: (context, state) {
+        /// Display error message if error occurred
+        if (state.status.isError) {
+          return const Center(
+            child: Text("An error occurred!"),
+          );
+        } else if (state.status.isLoading || state.status.isInitial) {
+          /// Display shimmer loading animation if the status is initial or loading
+          return Skeletonizer(
+            child: ListView.separated(
+              padding: const EdgeInsets.all(16),
+              itemBuilder: (context, index) => const BookCard(),
+              separatorBuilder: (_, __) => const SizedBox(height: 16),
+              itemCount: 32,
+            ),
+          );
+        }
+
+        /// Display empty message if the items is empty
+        if (state.items.isEmpty) {
+          return const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.folder_open),
+                SizedBox(height: 8),
+                Text("There is no books!"),
+              ],
+            ),
+          );
+        }
+
+        /// Otherwise, display the book data
+        return BlocConsumer<AddMyBooksBloc, AddMyBooksState>(
+          listener: (context, myBookState) {
+            if (myBookState.status.isError || myBookState.status.isSuccess) {
+              sl<GetBookBloc>().add(GetBookProccessed());
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(myBookState.message!),
+                ),
+              );
+            }
+          },
+          builder: (context, myBookState) {
+            return ListView.separated(
+              padding: const EdgeInsets.all(16),
+              itemBuilder: (context, index) => BookCard(
+                data: state.items.elementAt(index),
+              ),
+              separatorBuilder: (_, __) => const SizedBox(height: 16),
+              itemCount: state.items.length,
+            );
+          },
+        );
+      },
+    );
+  }
+}
