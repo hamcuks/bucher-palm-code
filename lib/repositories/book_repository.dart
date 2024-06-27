36f98da5fca03491fde67f7d9f4050e59957ad23
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-06-27 09:17:51 +0700

    feat(repositories): create book repository

---
+++ lib/repositories/book_repository.dart
@@ -0,0 +1,24 @@
+import 'package:bucher_palm_code/datasources/remote/book_remote_data_source.dart';
+import 'package:bucher_palm_code/models/book_model.dart';
+
+class BookRepository {
+  final BookRemoteDataSource _remote;
+
+  const BookRepository({
+    required BookRemoteDataSource remoteDataSource,
+  }) : _remote = remoteDataSource;
+
+  /// Return List of Book Model
+  ///
+  /// Params: optional page data
+  Future<List<BookModel>> getAll({int page = 1}) async {
+    try {
+      /// Get the book data from remote data source
+      final data = await _remote.getAll();
+
+      return data.results;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
