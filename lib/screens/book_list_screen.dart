950fb5f7126b51a1f7ed1aabbe981d177640bc1c
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-06-28 08:06:47 +0700

    feat(widgets): implement pagnation or infinite scroll

--- lib/screens/book_list_screen.dart
+++ lib/screens/book_list_screen.dart
@@ -1,39 +1,25 @@
-import 'package:bucher_palm_code/blocs/get_book/get_book_bloc.dart';
-import 'package:bucher_palm_code/screens/widgets/app_input_form.dart';
 import 'package:bucher_palm_code/screens/widgets/list_book_view.dart';
 import 'package:flutter/material.dart';
-
-import '../injector.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class BookListScreen extends StatelessWidget {
   const BookListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       bottom: false,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Books",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 24),
-                AppInputForm(
-                  hintText: "Find Books",
-                  onChanged: (val) => sl<GetBookBloc>().add(
-                    GetBookProccessed(search: val),
-                  ),
-                ),
-              ],
+            padding: EdgeInsets.all(16),
+            child: Text(
+              "Books",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
           ),
-          const Expanded(
+          Expanded(
             child: ListBookView(),
           ),
         ],
--- lib/screens/widgets/list_book_view.dart
+++ lib/screens/widgets/list_book_view.dart
@@ -1,76 +1,160 @@
 import 'package:bucher_palm_code/blocs/add_my_books/add_my_books_bloc.dart';
 import 'package:bucher_palm_code/blocs/get_book/get_book_bloc.dart';
 import 'package:bucher_palm_code/injector.dart';
+import 'package:bucher_palm_code/models/book_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
+import 'app_input_form.dart';
 import 'book_card.dart';
 
-class ListBookView extends StatelessWidget {
+class ListBookView extends StatefulWidget {
   final bool isFavorite;
 
   const ListBookView({super.key, this.isFavorite = false});
 
   @override
+  State<ListBookView> createState() => _ListBookViewState();
+}
+
+class _ListBookViewState extends State<ListBookView> {
+  static const _pageSize = 32;
+  int pageKey = 1;
+  String? search;
+
+  final PagingController<int, BookModel> _pagingController =
+      PagingController(firstPageKey: 1);
+
+  @override
+  void initState() {
+    /// Add pageing controller listener
+    ///
+    /// If there's any changes, call the GetBookProccessed event to get the new data
+    _pagingController.addPageRequestListener((pageKey) {
+      this.pageKey = pageKey;
+      sl<GetBookBloc>().add(GetBookProccessed(
+        page: pageKey,
+        isFavorite: widget.isFavorite,
+        search: search,
+      ));
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GetBookBloc, GetBookState>(
-      bloc: sl<GetBookBloc>()..add(GetBookProccessed(isFavorite: isFavorite)),
-      buildWhen: (previous, current) => previous != current,
-      builder: (context, state) {
-        /// Display error message if error occurred
-        if (state.status.isError) {
-          return const Center(
-            child: Text("An error occurred!"),
-          );
-        } else if (state.status.isLoading || state.status.isInitial) {
-          /// Display shimmer loading animation if the status is initial or loading
-          return Skeletonizer(
-            child: ListView.separated(
-              padding: const EdgeInsets.all(16),
-              itemBuilder: (context, index) => const BookCard(),
-              separatorBuilder: (_, __) => const SizedBox(height: 16),
-              itemCount: 32,
-            ),
-          );
-        }
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: AppInputForm(
+            hintText: "Find Books",
+            onChanged: (val) {
+              search = val;
+              _pagingController.refresh();
+            },
+          ),
+        ),
+        Expanded(
+          child: BlocConsumer<GetBookBloc, GetBookState>(
+            listener: (context, state) {
+              if (state.status.isSuccess) {
+                try {
+                  /// Check is the current page is last page or not
+                  final items = state.items;
+                  final isLastPage = items.length < _pageSize;
 
-        /// Display empty message if the items is empty
-        if (state.items.isEmpty) {
-          return const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.folder_open),
-                SizedBox(height: 8),
-                Text("There is no books!"),
-              ],
-            ),
-          );
-        }
+                  /// If last page, then append the items into last page of
+                  /// paging controller
+                  if (isLastPage) {
+                    _pagingController.appendLastPage(items);
+                  } else {
+                    /// Otherwise, append the items with the next page
+                    final nextPageKey = pageKey + 1;
 
-        /// Otherwise, display the book data
-        return BlocListener<AddMyBooksBloc, AddMyBooksState>(
-          listener: (context, myBookState) {
-            if (myBookState.status.isError || myBookState.status.isSuccess) {
-              sl<GetBookBloc>().add(GetBookProccessed(isFavorite: isFavorite));
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(myBookState.message!),
+                    _pagingController.appendPage(items, nextPageKey);
+                  }
+                } catch (e) {
+                  /// Trigger paging controller error if error occurred
+                  _pagingController.error = e;
+                }
+              }
+            },
+            builder: (context, state) {
+              return BlocListener<AddMyBooksBloc, AddMyBooksState>(
+                listener: (context, myBookState) {
+                  if (myBookState.status.isError ||
+                      myBookState.status.isSuccess) {
+                    sl<GetBookBloc>()
+                        .add(GetBookProccessed(isFavorite: widget.isFavorite));
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(myBookState.message!),
+                      ),
+                    );
+                  }
+                },
+                child: PagedListView<int, BookModel>.separated(
+                  pagingController: _pagingController,
+                  padding: const EdgeInsets.all(16),
+                  separatorBuilder: (_, __) => const SizedBox(height: 16),
+                  builderDelegate: PagedChildBuilderDelegate<BookModel>(
+                    /// Show book card loading when refreshing new page
+                    /// or load more item
+                    newPageProgressIndicatorBuilder: (_) => const Skeletonizer(
+                      child: BookCard(),
+                    ),
+
+                    /// Display empty message if the items is empty
+                    noItemsFoundIndicatorBuilder: (_) => const Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.folder_open),
+                          SizedBox(height: 8),
+                          Text("There is no books!"),
+                        ],
+                      ),
+                    ),
+
+                    /// Display error message if error occurred
+                    newPageErrorIndicatorBuilder: (_) => Center(
+                      child: Text(
+                        _pagingController.error ?? "An error occurred!",
+                      ),
+                    ),
+
+                    /// Display book card loading animation for the first time
+                    firstPageProgressIndicatorBuilder: (_) => Skeletonizer(
+                      child: Column(
+                        children: List.generate(
+                          10,
+                          (index) => const Column(
+                            children: [
+                              BookCard(),
+                              SizedBox(height: 16),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    /// Display the fetched book items
+                    itemBuilder: (context, item, index) {
+                      return BookCard(
+                        data: item,
+                      );
+                    },
+                  ),
                 ),
               );
-            }
-          },
-          child: ListView.separated(
-            padding: const EdgeInsets.all(16),
-            itemBuilder: (context, index) => BookCard(
-              data: state.items.elementAt(index),
-            ),
-            separatorBuilder: (_, __) => const SizedBox(height: 16),
-            itemCount: state.items.length,
+            },
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
